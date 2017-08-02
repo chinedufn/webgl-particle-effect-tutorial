ea3e1516d8943e7f7f0077ec9c5402d4989d8b63
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-08-03 07:32:34 +0900

    Create a billboard uniform

--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,5 @@
 var glMat4 = require('gl-mat4')
+var glMat3 = require('gl-mat3')
 
 // TODO: Make a particle texture atlas in photoshop
 // TODO: Particle effect tutorial lit tweet w/ gif of fire
@@ -23,7 +24,7 @@ attribute vec2 aTriCorner;
 attribute vec3 aCenterOffset;
 attribute vec3 aVelocity;
 uniform mat4 uPMatrix;
-uniform mat4 uViewMatrix;
+uniform mat4 uBillboard;
 
 varying float vLifetime;
 varying vec2 vTextureCoords;
@@ -42,7 +43,7 @@ void main (void) {
   float size = (vLifetime * vLifetime) * 0.05;
 
   position.xy += aTriCorner.xy * size;
-  gl_Position = uPMatrix * uViewMatrix * position;
+  gl_Position = uPMatrix * uBillboard * position;
 
   vTextureCoords = aTextureCoords;
   vLifetime = aLifetime;
@@ -115,6 +116,7 @@ var perspectiveUni = gl.getUniformLocation(shaderProgram, 'uPMatrix')
 var viewUni = gl.getUniformLocation(shaderProgram, 'uViewMatrix')
 var colorUni = gl.getUniformLocation(shaderProgram, 'uColor')
 var fireAtlasUni = gl.getUniformLocation(shaderProgram, 'uFireAtlas')
+var billboardUni = gl.getUniformLocation(shaderProgram, 'uBillboard')
 
 var imageIsLoaded = false
 var fireTexture = gl.createTexture()
@@ -233,7 +235,7 @@ gl.uniformMatrix4fv(perspectiveUni, false, glMat4.perspective([], Math.PI / 3, 1
  * Camera
  */
 var camera = glMat4.create()
-glMat4.translate(camera, camera, [0, 2, 1])
+glMat4.translate(camera, camera, [0, 1, 1])
 
 var cameraPos = [
   camera[12],
@@ -243,7 +245,18 @@ var cameraPos = [
 
 glMat4.lookAt(camera, cameraPos, startFirePos, [0, 1, 0])
 
-gl.uniformMatrix4fv(viewUni, false, camera)
+// https://swiftcoder.wordpress.com/2008/11/25/constructing-a-billboard-matrix/
+var cameraRotation = glMat3.fromMat4([], camera)
+glMat3.transpose(cameraRotation, cameraRotation)
+var cameraInverse = [
+  cameraRotation[0], cameraRotation[1], cameraRotation[2], 0,
+  cameraRotation[3], cameraRotation[4], cameraRotation[5], 0,
+  cameraRotation[6], cameraRotation[7], cameraRotation[8], 0,
+  0, 0, 0, 1
+]
+
+glMat4.multiply(camera, camera, cameraInverse)
+gl.uniformMatrix4fv(billboardUni, false, camera)
 
 /**
  * Draw
